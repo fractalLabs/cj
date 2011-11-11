b71eac2be62e6c9ea647302de9231a43419c2653
nex <andres@fractalmedia.mx>
2011-11-11 10:57:57 -0500

    wrote README, etc

--- project.clj
+++ project.clj
@@ -2,8 +2,5 @@
   :description "Lib aimed at increasing entropy in clojure developements"
   :main cj.shell
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [fm/fm "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[mycroft/mycroft "0.0.2"]]
-  :repositories {"fractal" {:url "http://67.205.67.146:8099/archiva/repository/fractal"}, 
-                 "snapshots" {:url "http://67.205.67.146:8099/archiva/repository/snapshots"}})
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[mycroft/mycroft "0.0.2"]])
