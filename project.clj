43df1db05a9e45b7f5777a61296ea137b8f3002d
patz <jorge@fractalmedia.mx>
2012-12-13 18:01:14 -0600

    ya no es snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cj "1.0.0-SNAPSHOT"
+(defproject cj "1.0.0"
   :description "Lib aimed at increasing entropy in clojure developements"
   :main cj.shell
   :dependencies [[org.clojure/clojure "1.4.0"]])
